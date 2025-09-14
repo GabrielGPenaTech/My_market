40513cb9d6c59d21becf1892209362bc9743519a
GabrielGPena <kbeca.gabriel@hotmail.com>
2025-09-14 06:09:01 -0300

    Add Filter component and integrate lucide-react-native icons
    - Introduced Filter component to manage filter statuses.
    - Added StatusIcon component for visual representation of filter states.
    - Updated App component to include filter functionality.
    - Enhanced styles for header and filter elements.
    - Added lucide-react-native as a dependency.

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,8 +1,12 @@
-import { Image, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 import { styles } from './styles'
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
+import { Filter } from '@/components/Filter';
+import { FilterStatus } from '@/types/FilterStatus';
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.DONE, FilterStatus.PENDING]
 
 export  function App() {
 
@@ -15,7 +19,19 @@ export  function App() {
           <Button text='Adicionar' onPress={() => {console.log("alou")}} activeOpacity={0.8} />
         </View>
 
-        <View style={styles.content}></View>
+        <View style={styles.content}>
+          <View style={styles.header}>
+            {
+              FILTER_STATUS.map((status) => 
+                <Filter key={status} isActive={status === FilterStatus.DONE}  status={status}/>
+              )
+            }
+
+            <TouchableOpacity style={styles.clearButton}>
+              <Text style={styles.clearText}>Limpar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
     </View>
   );
 }
